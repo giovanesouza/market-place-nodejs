ff678f5d1890c5b6c017b8f1e96ee651ccfb0684
Giovane Souza <developergiovanesouza@gmail.com>
2023-11-07 19:32:58 -0300

    fix: correção no método de exclusão

--- src/service/usuario.service.js
+++ src/service/usuario.service.js
@@ -19,7 +19,7 @@ const updateUserService = (id, body) => {
 };
 
 const removeUserService = (id) => {
-    return Usuario.findByIdAndRemove(id);
+    return Usuario.findByIdAndDelete(id);
 };
 
 const addUserAddressService = (id, endereco) => {
