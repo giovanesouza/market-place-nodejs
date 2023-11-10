3afa0c5eba2244b97cd88360da07f1c99821d9fb
Giovane Souza <developergiovanesouza@gmail.com>
2023-11-10 17:21:52 -0300

    feat: validação no usuário, categoria e produto

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -3,23 +3,24 @@ const router = express.Router();
 const usuarioController = require('../controller/usuario.controller');
 
 const authMiddleware = require('../middleware/auth.middleware');
+const { validaUsuario } = require('../middleware/validacao.middleware');
 
 // Rotas GET
 router.get('/findById/:id', authMiddleware, usuarioController.findUserByIdController);
-router.get('/findAll', usuarioController.findAllUsersController);
+router.get('/findAll', authMiddleware, usuarioController.findAllUsersController);
 
 // Rotas POST
-router.post('/create', usuarioController.createUserController);
-router.post('/addAddress/:id', usuarioController.addUserAddressController);
-router.post('/addFavProduct/:id', usuarioController.addUserFavProductController);
+router.post('/create', validaUsuario, usuarioController.createUserController);
+router.post('/addAddress/:id', authMiddleware, usuarioController.addUserAddressController);
+router.post('/addFavProduct/:id', authMiddleware, usuarioController.addUserFavProductController);
 
 // Rotas PUT
-router.put('/update/:id', usuarioController.updateUserController);
+router.put('/update/:id', authMiddleware, validaUsuario, usuarioController.updateUserController);
 
 // Rotas DELETE
-router.delete('/remove/:id', usuarioController.removeUserController);
-router.delete('/removeAddress', usuarioController.removeUserAddressController);
-router.delete('/removeFavProduct/:id', usuarioController.removeUserFavProductController);
+router.delete('/remove/:id', authMiddleware, usuarioController.removeUserController);
+router.delete('/removeAddress', authMiddleware, usuarioController.removeUserAddressController);
+router.delete('/removeFavProduct/:id', authMiddleware, usuarioController.removeUserFavProductController);
 
 
 module.exports = router;
